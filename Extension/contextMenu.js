4cffcc4805d625a5e3172fc2d0af5f004db2fb52
einhar1 <77612217+einhar1@users.noreply.github.com>
2022-05-13 14:40:14 +0200

    diverse förbättringar

--- Extension/contextMenu.js
+++ Extension/contextMenu.js
@@ -1,6 +1,9 @@
+var sökord;
+
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId == "search") {
         console.log(info.selectionText);
+        sökord = info.selectionText;
         sendToServer(info.selectionText);
     }
 });
@@ -8,15 +11,36 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 chrome.runtime.onMessage.addListener(
     function (request) {
         console.log("Input:" + request.sökord);
+        sökord = request.sökord;
         sendToServer(request.sökord);
     }
 );
+
+chrome.notifications.onClicked.addListener(
+    function () {
+        console.log("click");
+        console.log(chrome.tabs.query({
+            currentWindow: true,
+            url: "https://svenska.se/*"
+        }));
+        
+        chrome.tabs.query({ currentWindow: true, url: "https://svenska.se/*" }, function (tab) {
+            if (tab.length != 0) {
+                chrome.tabs.update(tab[0].id, { url: 'https://svenska.se/tre/?sok=' + sökord + '&pz=1', highlighted: true });
+            } else {
+                chrome.tabs.create({
+                    url: 'https://svenska.se/tre/?sok=' + sökord + '&pz=1'
+                });
+            }
+        });
+    }
+)
 
 function sendToServer(input) {
 
     const data = { sökord: input };
 
-    chrome.notifications.clear('betydelse');
+    chrome.notifications.clear("betydelse");
 
     fetch('http://127.0.0.1:3000/api', {
         method: 'POST', // or 'PUT'
@@ -30,16 +54,20 @@ function sendToServer(input) {
                 iconUrl: 'icons/icon_large.png',
                 title: input,
                 message: data,
-                buttons: [
-                    {
-                        title: 'Öppna i ny flik'
-                    }
-                ],
-                priority: 2
+                requireInteraction: true,
+                priority: 2,
+                silent: true
             })
         })
         .catch((error) => {
             console.error('Error:', error);
+            chrome.notifications.create('betydelse', {
+                type: 'basic',
+                iconUrl: 'icons/icon_large.png',
+                title: 'Fel',
+                message: 'Något gick fel',
+                priority: 2
+            })
         });
 }
 
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Chrome-tillägg som hjälper en att slå upp ord. Använder Svensk ordbok utgiven av Svenska Akademien (SO).
 
-Node.js krävs för att köra servern. För att använda tillägget, lägg till det i chrome genom att sätta på programmererläge.
+Tillägget använder en Node.js server. Installera Node.js för att kunna köra servern. För att använda tillägget, lägg till det i chrome som ett okomprimerat tillägg.
+
+Om det står "något gick fel" betyder det antingen att det inte fanns några sökresultat eller att något blev fel i koden, men då är det bara att försöka igen så funkar det antagligen.
+
+På grund av rättsliga skäl kan jag inte publicera det här tillägget på Chrome Web Store då jag inte har tillåtelse att använda mig av Svenska Akademins ordböcker.
